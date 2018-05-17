80d537a6dd0b9c1450025f0144fdc70ba2880a25
770363647 <z770363647@126.com>
2018-05-17 11:17:45 +0800

    Initial commit

---
+++ app/src/main/java/com/zhaojy/photoselect/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zhaojy.photoselect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author zhaojy
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ selectlibrary/src/main/java/com/zhaojy/selectlibrary/data/GetPhotoPath.java
@@ -0,0 +1,12 @@
+package com.zhaojy.selectlibrary.data;
+
+/**
+ * 获取照片路径
+ *
+ * @author: zhaojy
+ * @data:On 2018/5/17.
+ */
+
+public class GetPhotoPath {
+
+}
